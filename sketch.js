aee8676d941590a207193fa0e561b4b33e4daf24
Adam Iannazzone <46025473+jiannazzone@users.noreply.github.com>
2020-04-14 11:07:56 -0400

    Update sketch.js
    Redid the layout. Hopefully the cards will be more readable over a video stream. The canvas also stays square now, so you can have another window next to it. The size of the square will always match the height of your window.

--- sketch.js
+++ sketch.js
@@ -8,10 +8,11 @@ let button;
 let rolls = [1, 2, 3, 4, 5, 6];
 
 //positions
-let cardX = [];
-let cardY;
-let diceX = [];
-let diceY; 
+let cardX;
+let cardY = [];
+let diceX;
+let diceY = [];
+let diceSize;
 
 function preload() {
   // Load images for later use and create copy to edit as you go
@@ -27,7 +28,8 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  createCanvas(windowHeight, windowHeight);
   currentCards = shuffle(allCards);
   makeScaling();
   addDecorations();
@@ -36,78 +38,85 @@ function setup() {
 
 function makeScaling() {
   // scale cards to screen size
-  cardWidth = width/4;
+  cardWidth = width / 2.5;
   cardHeight = cardWidth * 0.66;
-  spacing = width/16;
-  cardY = spacing/1.25;
-  
-  //generate x-coordinates for the cards
-  let x = spacing;
+  spacing = height / 32;
+
+  //generate y-coordinates for the cards
+  cardX = 2 * spacing + cardWidth / 2;
+  let y = height / 2 - spacing / 2 - cardHeight;
   for (let i = 0; i < 3; i++) {
-    cardX.push(x);
-    x += cardWidth + spacing;
+    cardY.push(y);
+    y += cardHeight + spacing / 2;
   }
-  
+
   // scale dice to screen size
-  diceY = (9/12)*height;
-  x = width/2 - height/4;
+  diceSize = width / 5;
+  diceX = (3 / 4) * width;
+  y = height / 2 - spacing - diceSize
   for (let i = 0; i < 3; i++) {
-    diceX.push(x);
-    x += height/4;
+    diceY.push(y);
+    y += diceSize + spacing;
   }
 }
+
 function makeDice() {
   rectMode(CENTER);
-  let diceSize = height/6;
   fill(255);
   stroke(0);
   strokeWeight(5);
   for (let i = 0; i < 3; i++) {
-    rect(diceX[i], diceY, diceSize, diceSize, 20);
+    rect(diceX, cardY[i], diceSize, diceSize, 20);
   }
 }
+
 function makeButton() {
+  stroke(0);
+  //strokeWeight(2);
   textAlign(CENTER, CENTER);
   rectMode(CENTER);
   fill(75);
-  rect(width/2, height/2, width/3, height/8, 20);
+  rect(width / 2, height - spacing / 2, width / 1.25, height / 10, 20);
   fill(255);
-  stroke(0);
-  strokeWeight(3);
   textSize(26);
-  text("Deal 3 new cards and roll 3d6", width/2, height/2, width/3, height/6);
+  text("Click to deal 3 new cards and roll 3d6", width / 2, height - spacing, width / 1.5, height / 10);
 }
+
 function addDecorations() {
+  stroke(0);
+  strokeWeight(2);
   textAlign(CENTER, TOP);
-  textSize(32);
+  textSize(24);
   rectMode(CENTER);
-  background(60);
+  background(75);
   fill(200);
-  rect(width/2, height/2, width-spacing, height-spacing);
+  rect(width / 2, height / 2, width - spacing / 2, height - spacing / 2);
+  fill(75)
+  rect(width / 2, spacing / 2, width / 1.25, height / 10, 20)
   fill(255);
-  text('\"Roll to Escape\" by Story Machine Games', width/2, 0);
+  text('\"Roll to Escape\" by Story Machine Games', width / 2, spacing / 2);
 }
+
 function dealAndRoll() {
+  rectMode(CENTER);
   makeDice();
   textSize(64);
   textAlign(CENTER, CENTER);
-  if (currentCards.length == 0){
+  if (currentCards.length == 0) {
     currentCards = shuffle(allCards);
   }
-  for (let i = 0; i < 3 ; i++) {
+  for (let i = 0; i < 3; i++) {
     let thisCard = currentCards.pop();
-    rectMode(CORNER);
-    image(thisCard, cardX[i], cardY, cardWidth, cardHeight);
-    rectMode(CENTER);
+    image(thisCard, cardX - cardWidth / 2, cardY[i] - cardHeight / 2, cardWidth, cardHeight);
     fill(0);
     noStroke();
-    text(random(rolls), diceX[i], diceY);
+    text(random(rolls), diceX, cardY[i]);
   }
 }
+
 function mousePressed() {
-  if (mouseX >= width/3 && mouseX <= (2/3)*width) {
-    if (mouseY >= (5/12)*height && mouseY <= (7/12)*height) {
-      dealAndRoll();
-    }
+  if (mouseY > (9 / 10) * height - spacing / 2 &&
+    mouseX > width / 10 && mouseX < (9 / 10) * width) {
+    dealAndRoll();
   }
 }
